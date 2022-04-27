6ac43ccfd1517f816f9309e370c3bafdf51bfbdb
Antonio Lopez <pezlop202@gmail.com>
2022-04-26 20:31:19 -0700

    init layout mobile-first approach

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        desaturatedRed: 'hsl(0, 36%, 70%)',
+        softRed: 'hsl(0, 93%, 68%)',
+        darkGrayishRed: 'hsl(0, 6%, 24%)',
+      },
+      fontFamily: {
+        'sans-serif': ['Josefin Sans'],
+      },
+    },
+  },
+  plugins: [],
+};
