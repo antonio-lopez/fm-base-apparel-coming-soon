73b09e1bf00a0588516ecbf02485db704affa27a
Antonio Lopez <pezlop202@gmail.com>
2022-04-27 18:26:06 -0700

    mobile view finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
         desaturatedRed: 'hsl(0, 36%, 70%)',
         softRed: 'hsl(0, 93%, 68%)',
         darkGrayishRed: 'hsl(0, 6%, 24%)',
+        bgWhite: 'hsl(0, 0%, 100%)',
+        bgPink: 'hsl(0, 100%, 98%)',
+        softPink: 'hsl(0, 80%, 86%)',
+        darkPink: 'hsl(0, 74%, 74%)',
       },
       fontFamily: {
         'sans-serif': ['Josefin Sans'],
